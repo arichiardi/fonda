c64d759d718337f364657d3b199c447e03b27a60
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-11-16 12:24:33 -0800

    Add circleci machinery (#2)
    * Added circle-ci yml file in order to automatically deploy to clojars when a
    
    * Read project.clj version from yarn
    
    * Fixed various stuff

--- project.clj
+++ project.clj
@@ -1,4 +1,20 @@
-(defproject elasticpath/fonda "0.0.1"
+(defn throw-if-non-zero
+  [process]
+  (let [exit-code (.waitFor process)]
+    (if-not (= 0 exit-code)
+      (throw (ex-info (str "Process returned non-zero exit code (" exit-code ")")
+                      {:exit-code exit-code}))
+      process)))
+
+(def +version+
+  (-> (ProcessBuilder. ["yarn" "version" "--version"])
+      (.start)
+      (throw-if-non-zero)
+      (.getInputStream)
+      slurp
+      (clojure.string/trim-newline)))
+
+(defproject com.elasticpath/fonda +version+
   :url "https://github.com/elasticpath/fonda"
   :source-paths ["src"]
   :test-paths ["test"]
@@ -8,4 +24,6 @@
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev :test]})
+                           :aliases [:dev :test]}
+  :repositories [["releases" {:username :env/clojars_username
+                              :password :env/clojars_password}]])
