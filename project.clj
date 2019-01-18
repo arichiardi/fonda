96364ce4c2a9524492569a91a8a8bc9e6f80e60d
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-01-18 15:47:47 -0800

    Snapshot 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.elasticpath/fonda "0.1.2"
+(defproject com.elasticpath/fonda "0.2.1-SNAPSHOT"
   :url "https://github.com/elasticpath/fonda"
   :description "An async pipeline approach to functional core - imperative shell."
   :license {:name "Apache License"
